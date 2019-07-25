2053237525af6f96bd5c61a43f1f643b4409176f
alparslancay <alparslancay1@gmail.com>
2019-07-25 23:58:46 +0300

    Added GameFeatures

---
+++ HeadsOrTailsWarGame/GameFeatures/StateBorders.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameFeatures
+{
+    public class StateBorders
+    {
+        Button[] currentButtons = new Button[900];
+        StateColor stateColor = new StateColor();
+
+        public Button[] CreateButtons()
+        {
+            const int FIRSTXCOORDINATE = 60;
+            int xCoordinate = FIRSTXCOORDINATE;
+            int yCoordinate = 0;
+            int colorController = 0;
+
+            for (int buttonRecorder = 0; buttonRecorder < currentButtons.Length; buttonRecorder++)
+            {
+                if (buttonRecorder == 450) colorController = 1;
+
+                CreateButtonProperties(buttonRecorder, xCoordinate, yCoordinate, stateColor.TakeColor(FindStateNumber(6,buttonRecorder)));
+
+
+                if (xCoordinate - FIRSTXCOORDINATE >= 580)
+                {
+                    yCoordinate += 20;
+                    xCoordinate = FIRSTXCOORDINATE;
+
+                }
+                else { xCoordinate += 20; }
+
+            }
+
+            return currentButtons;
+        }
+
+        private void CreateButtonProperties(int buttonNumber, int xCoordinate, int yCoordinate, Color buttonColor)
+        {
+            Button gameButton = new Button
+            {
+                Size = new Size(20, 20),
+                Location = new Point(xCoordinate, yCoordinate),
+                BackColor = buttonColor,
+                Name = buttonNumber.ToString()
+            };
+
+            currentButtons[buttonNumber] = gameButton;
+        }
+
+        private int FindStateNumber(int numberPlayers, int buttonNumber)
+        {
+            int aSidePlayerNumbers = (numberPlayers - numberPlayers % 2) / 2;
+            int bSidePlayerNumbers = aSidePlayerNumbers + numberPlayers % 2;
+
+            int aSideAreaPerRows = 30 / aSidePlayerNumbers;
+            int bSideAreaPerRows = 30 / bSidePlayerNumbers;
+
+            if(buttonNumber < 450)
+            {
+                return buttonNumber % 30 / bSideAreaPerRows;
+                
+            }
+
+            else
+            {
+                return (buttonNumber % 30 / aSideAreaPerRows) + aSidePlayerNumbers;
+            }
+        }
+    }
+}
---
+++ HeadsOrTailsWarGame/GameFeatures/StateColor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFeatures
+{
+    public class StateColor
+    {
+        public Color TakeColor(int colorNumber)
+        {
+            if (colorNumber < 3) return TakeColorUpperRegion(colorNumber);
+            else return TakeColorLowerRegion(colorNumber - 3);
+        }
+
+        public Color TakeColorUpperRegion(int colorNumber)
+        {
+            switch (colorNumber)
+            {
+                case 0:
+                    return Color.Red;
+                case 1:
+                    return Color.Blue;
+                default:
+                    return Color.Yellow;
+
+            }
+        }
+
+        public Color TakeColorLowerRegion(int colorNumber)
+        {
+            switch (colorNumber)
+            {
+                case 0:
+                    return Color.Purple;
+                case 1:
+                    return Color.Green;
+                default:
+                    return Color.Orange;
+
+            }
+        }
+    }
+}
