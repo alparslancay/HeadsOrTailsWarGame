8829d0828be818cdf114977634dafd0938a4618f
alparslancay <alparslancay1@gmail.com>
2019-07-26 23:30:50 +0300

    Added Player Object in the Game

---
+++ HeadsOrTailsWarGame/GameFeatures/GamePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFeatures
+{
+    public class GamePlayer
+    {
+        public string StateName { get; set; }
+        public List<int> OwnedArea { get; set; }
+    }
+}
