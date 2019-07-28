8d41b9fa76b09fb3f26e4eb59a51592a0419129b
alparslancay <alparslancay1@gmail.com>
2019-07-28 22:37:43 +0300

    Added ButtonSelector Class

---
+++ HeadsOrTailsWarGame/GameFeatures/ButtonSelector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameFeatures
+{
+    public class ButtonSelector
+    {
+        Stack<ButtonInformationSaver> selectedButtonInformations = new Stack<ButtonInformationSaver>();
+
+        Button[] currentButtons;
+
+        public ButtonSelector(Button [] stateButtons)
+        {
+            currentButtons = stateButtons;
+        }
+
+        private bool StackIsEmpty()
+        {
+            return selectedButtonInformations.Count == 0;
+        }
+
+        public void SelectButton(Button oldClickedButton)
+        {
+            if (!StackIsEmpty() && oldClickedButton.BackColor != selectedButtonInformations.First().currentColor) MessageBox.Show("You can not select from another state!");
+
+             else
+             {
+                 ButtonInformationSaver saverButton = new ButtonInformationSaver()
+                 {
+                     currentColor = oldClickedButton.BackColor,
+                     buttonNumber = int.Parse(oldClickedButton.Name)
+                 };
+                 selectedButtonInformations.Push(saverButton);
+
+                 oldClickedButton.BackColor = Color.Black;
+             }
+        }
+
+        public void ResetSelections()
+        {
+            while (selectedButtonInformations.Count != 0)
+            {
+                ButtonInformationSaver oldButtonProperties = selectedButtonInformations.Pop();
+                currentButtons[oldButtonProperties.buttonNumber].BackColor = oldButtonProperties.currentColor;
+            }
+        }
+    }
+}
