382d9271fa8e9992c17794da9f9f953c5328dd49
alparslancay <alparslancay1@gmail.com>
2019-07-31 12:48:25 +0300

    Added Main Form

---
+++ HeadsOrTailsWarGame/HeadsOrTailsWarGame/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace HeadsOrTailsWarGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmb_NumberPlayers = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(210, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Choose Number Players";
+            // 
+            // cmb_NumberPlayers
+            // 
+            this.cmb_NumberPlayers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_NumberPlayers.FormattingEnabled = true;
+            this.cmb_NumberPlayers.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.cmb_NumberPlayers.Location = new System.Drawing.Point(454, 200);
+            this.cmb_NumberPlayers.Name = "cmb_NumberPlayers";
+            this.cmb_NumberPlayers.Size = new System.Drawing.Size(121, 24);
+            this.cmb_NumberPlayers.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(220, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(355, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Heads or Tails War Game!";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.BackColor = System.Drawing.Color.Orange;
+            this.btn_Play.Font = new System.Drawing.Font("Segoe Script", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Play.Location = new System.Drawing.Point(331, 285);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(142, 48);
+            this.btn_Play.TabIndex = 4;
+            this.btn_Play.Text = "Play!";
+            this.btn_Play.UseVisualStyleBackColor = false;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Play);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmb_NumberPlayers);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmb_NumberPlayers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Play;
+    }
+}
---
+++ HeadsOrTailsWarGame/HeadsOrTailsWarGame/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadsOrTailsWarGame
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Play_Click(object sender, EventArgs e)
+        {
+            if (cmb_NumberPlayers.Text != "")
+            {
+                GameDisplay gameDisplay = new GameDisplay(int.Parse(cmb_NumberPlayers.Text));
+                gameDisplay.ShowDialog();
+            }
+
+            else MessageBox.Show("Please choose number of players!");
+        }
+        
+    }
+}
