1b3b128dab434f5c01fb7a7f2d53e454cf98356a
alparslancay <alparslancay1@gmail.com>
2019-08-02 23:55:17 +0300

    Added Finish Controller

---
+++ HeadsOrTailsWarGame/GameFeatures/FinishController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFeatures
+{
+    public class FinishController
+    {
+        GameStates[] gameStates;
+
+        public FinishController(GameStates[] gameStates)
+        {
+            this.gameStates = gameStates;
+        }
+
+        public bool IsWinner(int stateNumber)
+        {
+            return gameStates[stateNumber].OwnedArea.Count == 900;
+        }
+    }
+}
