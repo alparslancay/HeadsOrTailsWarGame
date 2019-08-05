e6e000163eef5a66b77b1ab9dbcb9c36dc110e2e
alparslancay <alparslancay1@gmail.com>
2019-08-05 21:56:02 +0300

    Added Bet Algorithm For If Lose A War

--- HeadsOrTailsWarGame/GameFeatures/ButtonSelector.cs
+++ HeadsOrTailsWarGame/GameFeatures/ButtonSelector.cs
@@ -10,7 +10,8 @@ namespace GameFeatures
 {
     public class ButtonSelector
     {
-        Stack<ButtonInformationSaver> selectedButtonInformations = new Stack<ButtonInformationSaver>();
+        Stack<ButtonInformationSaver> selectedTakeOverAreasInformations = new Stack<ButtonInformationSaver>();
+        Stack<ButtonInformationSaver> selectorBetAreaInformations = new Stack<ButtonInformationSaver>();
         StateAreas stateAreas = StateAreas.GetStateAreasClass();
 
         Button[] currentButtons;
@@ -24,16 +25,16 @@ namespace GameFeatures
 
         private bool StackIsEmpty()
         {
-            return selectedButtonInformations.Count == 0;
+            return selectedTakeOverAreasInformations.Count == 0;
         }
         
-        public void SelectButton(Button oldClickedButton, int selectorPlayerNumber)
+        public void SelectOtherStateArea(Button oldClickedButton, int selectorPlayerNumber)
         {
             if (!IsSelectorPlayerState(oldClickedButton, selectorPlayerNumber))
 
                 if (!IsStateOfAnotherEnemyPlayer(oldClickedButton))
 
-                    if (IsAreaAdjacent(oldClickedButton, selectorPlayerNumber))
+                    if (IsAreaAdjacent(oldClickedButton, selectorPlayerNumber, selectedTakeOverAreasInformations))
                     {
 
                         ButtonInformationSaver saverButton = new ButtonInformationSaver()
@@ -43,7 +44,7 @@ namespace GameFeatures
                             buttonNumber = int.Parse(oldClickedButton.Name)
                         };
 
-                        selectedButtonInformations.Push(saverButton);
+                        selectedTakeOverAreasInformations.Push(saverButton);
 
                         oldClickedButton.BackColor = Color.Black;
                     }
@@ -55,9 +56,66 @@ namespace GameFeatures
 
         }
 
-        public Stack<ButtonInformationSaver> GetSelectedAreas()
+        public void SelectSelectorStateArea(Button oldClickedButton, int selectorPlayerNumber)
         {
-            return selectedButtonInformations;
+            int selectedStateNumber = selectedTakeOverAreasInformations.First().ownedPlayerNumber;
+
+            if (IsSelectorPlayerState(oldClickedButton, selectorPlayerNumber))
+
+                if (IsAreaAdjacent(oldClickedButton, selectedStateNumber, selectorBetAreaInformations))
+                {
+
+                    ButtonInformationSaver saverButton = new ButtonInformationSaver()
+                    {
+                        ownedPlayerNumber = stateColor.GetPlayerNumberWithColor(oldClickedButton.BackColor),
+                        currentColor = oldClickedButton.BackColor,
+                        buttonNumber = int.Parse(oldClickedButton.Name)
+                    };
+
+                    selectorBetAreaInformations.Push(saverButton);
+
+                    oldClickedButton.BackColor = Color.White;
+                }
+                else MessageBox.Show("You can only select adjacent areas!");
+
+            else MessageBox.Show("You have to select your state!");
+
+        }
+
+        public void RemoveTakeOverArea(Button selectedButton)
+        {
+            Stack<ButtonInformationSaver> currentTakeOverAreasInformations = new Stack<ButtonInformationSaver>();
+
+            foreach (var currentAreaInformations in selectedTakeOverAreasInformations)
+            {
+                if (currentAreaInformations.buttonNumber != int.Parse(selectedButton.Name))
+                    currentTakeOverAreasInformations.Push(currentAreaInformations);
+            }
+
+            selectedTakeOverAreasInformations = currentTakeOverAreasInformations;
+        }
+
+        public void RemoveBetArea(Button selectedButton)
+        {
+            Stack<ButtonInformationSaver> currentBetAreasInformations = new Stack<ButtonInformationSaver>();
+
+            foreach (var currentAreaInformations in selectorBetAreaInformations)
+            {
+                if (currentAreaInformations.buttonNumber != int.Parse(selectedButton.Name))
+                    currentBetAreasInformations.Push(currentAreaInformations);
+            }
+
+            selectedTakeOverAreasInformations = currentBetAreasInformations;
+        }
+
+        public Stack<ButtonInformationSaver> GetTakeOverAreas()
+        {
+            return selectedTakeOverAreasInformations;
+        }
+
+        public Stack<ButtonInformationSaver> GetSelectorBetAreas()
+        {
+            return selectorBetAreaInformations;
         }
 
 
@@ -68,38 +126,46 @@ namespace GameFeatures
 
         private bool IsStateOfAnotherEnemyPlayer(Button oldClickedButton)
         {
-            return !StackIsEmpty() && oldClickedButton.BackColor != selectedButtonInformations.First().currentColor;
+            return !StackIsEmpty() && oldClickedButton.BackColor != selectedTakeOverAreasInformations.First().currentColor;
         }
         
-        private bool IsAreaAdjacent(Button currentArea, int stateNumber)
+        private bool IsAreaAdjacent(Button currentArea, int stateNumber, Stack<ButtonInformationSaver> selectedAreas)
         {
-            if (StackIsEmpty())
+            if (selectedAreas.Count == 0)
                 return stateAreas.IsAdjacentToTheAreas(currentArea, stateAreas.GetStateEndZones(stateNumber));
 
             else
             {
-                List<Button> selectedAreas = new List<Button>();
+                List<Button> selectedAreasList = new List<Button>();
 
-                foreach (var currentSelectedAreaInformation in selectedButtonInformations)
+                foreach (var currentSelectedAreaInformation in selectedAreas)
                 {
-                    selectedAreas.Add(currentButtons[currentSelectedAreaInformation.buttonNumber]);
+                    selectedAreasList.Add(currentButtons[currentSelectedAreaInformation.buttonNumber]);
                 }
 
-                return stateAreas.IsAdjacentToTheSelectedAreas(currentArea,selectedAreas);
+                return stateAreas.IsAdjacentToTheSelectedAreas(currentArea,selectedAreasList);
             }
             
         }
-
         
-        
-
         public void ResetSelections()
         {
-            while (selectedButtonInformations.Count != 0)
+            while (selectedTakeOverAreasInformations.Count != 0)
             {
-                ButtonInformationSaver oldButtonProperties = selectedButtonInformations.Pop();
+                ButtonInformationSaver oldButtonProperties = selectedTakeOverAreasInformations.Pop();
                 currentButtons[oldButtonProperties.buttonNumber].BackColor = oldButtonProperties.currentColor;
             }
+
+            while(selectorBetAreaInformations.Count != 0)
+            {
+                ButtonInformationSaver oldButtonProperties = selectorBetAreaInformations.Pop();
+                currentButtons[oldButtonProperties.buttonNumber].BackColor = oldButtonProperties.currentColor;
+            }
+        }
+
+        public int GetSelectedStateNumber()
+        {
+            return selectedTakeOverAreasInformations.First().ownedPlayerNumber;
         }
     }
 }
