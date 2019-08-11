9953ba9f3b84605d618e3cdf5aa13953cf15db3c
alparslancay <alparslancay1@gmail.com>
2019-08-11 20:39:55 +0300

    Added Entities Layer

---
+++ HeadsOrTailsWarGame/GameEntities/AreaSelectNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEntities
+{
+    public class AreaSelectNode
+    {
+        public int ownedStateNumber;
+        public object currentFlag;
+        public int areaNumber;
+    }
+}
---
+++ HeadsOrTailsWarGame/GameEntities/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEntities
+{
+    public class GameState
+    {
+        public List<object> ownedAreas;
+        public string name;
+        public object flag;
+    }
+}
