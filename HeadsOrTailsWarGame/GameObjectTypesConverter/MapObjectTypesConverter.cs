27b1f3f6676552b67fd074095cbf400b777868be
alparslancay <alparslancay1@gmail.com>
2019-08-11 20:39:09 +0300

    Added Object Converter For Easy Maintain

---
+++ HeadsOrTailsWarGame/GameObjectTypesConverter/MapObjectTypesConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameObjects
+{
+    public static class MapObjectTypesConverter
+    {
+        public static Button ConvertMapAreaObjectType(object convertingObject)
+        {
+            return (Button)convertingObject;
+        }
+    }
+}
---
+++ HeadsOrTailsWarGame/GameObjectTypesConverter/StateObjectTypesConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameObjectTypes
+{
+    public static class StateObjectTypesConverter
+    {
+        public static Color ConvertStateFlagObjectType(object convertingObject)
+        {
+            return (Color)convertingObject;
+        }
+    }
+}
