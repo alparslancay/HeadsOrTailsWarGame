df0a3ed9ba6488e29071ea2b31efffb9623fd60e
alparslancay <alparslancay1@gmail.com>
2019-08-15 23:01:16 +0300

    Added Messager For Winning/Losing Statement

---
+++ HeadsOrTailsWarGame/GameFeatures/GameStatementMessager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFeatures
+{
+    public static class GameStatementMessager
+    {
+        public static string statementMessage;
+
+        public static void WinMessage(string winnerName)
+        {
+            statementMessage = winnerName + " has won!";
+        }
+
+        public static void LoseMessage(string loserName)
+        {
+            statementMessage = loserName + " has lost!";
+        }
+    }
+}
